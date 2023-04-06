8879d207a41b529d60fb6a3953b15b27dacf9c43
HugoVanier64 <hugo.vanier@etu.estia.fr>
2023-04-06 12:59:31 +0200

    nouvelle version

---
+++ src/main/java/estia/esabot/esabot/EsabotApplication.java
@@ -0,0 +1,14 @@
+package estia.esabot.esabot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import controllers.SabotController;
+
+@SpringBootApplication
+public class EsabotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsabotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/estia/esabot/esabot/EsabotApplicationTests.java
@@ -0,0 +1,13 @@
+package estia.esabot.esabot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsabotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
