1f7e919e95c9fbe6324ec1e07d9ad0c0efcd8e40
HugoVanier64 <hugo.vanier@etu.estia.fr>
2023-04-12 12:05:09 +0200

    ça va marcher

--- src/main/java/estia/esabot/esabot/EsabotApplication.java
+++ src/main/java/estia/esabot/esabot/EsabotApplication.java
@@ -2,7 +2,9 @@ package estia.esabot.esabot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import controllers.SabotController;
+import org.springframework.context.annotation.ComponentScan;
+
+@ComponentScan
 
 @SpringBootApplication
 public class EsabotApplication {
